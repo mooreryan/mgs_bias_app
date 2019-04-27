3cd543de968a1df14d1c82926645eea96b548a55
Ryan Moore <moorer@udel.edu>
2019-04-26 20:12:14 -0400

    Add bar charts

--- src/mgs_bias/core.cljs
+++ src/mgs_bias/core.cljs
@@ -1,5 +1,6 @@
 (ns mgs-bias.core
   (:require
+   [cljs.pprint :as pp]
    [goog.string :as gstring]
    [goog.string.format]
    [goog.dom :as gdom]
@@ -15,6 +16,7 @@
 (def html-id-otu-table-observed-counts "mgs-bias-otu-table-observed-counts")
 (def html-id-composition-charts "mgs-bias-composition-charts")
 (def html-id-composition-charts-svg "mgs-bias-composition-charts-svg")
+(def html-id-update-charts-button "mgs-bias-update-charts")
 
 (def config (atom {:num-otus 3}))
 
@@ -29,21 +31,26 @@
 
 ;; row i is OTU i, col j is sample j
 (def actual-otu-counts
-  (r/atom [[1 15]
-           [1 1]
-           [1 4]]))
+  (r/atom [[1 15] ; OTU 1
+           [1  1] ; OTU 2
+           [1  4] ; OTU 3
+           ]))
 
 (def observed-otu-counts
   (r/atom nil))
 
 ;; row i is OTU i, col j is protocol step j
 (def protocol-otu-bias-per-step
-  (r/atom [[1  1 1    1]
-           [4  6 0.5  1.5]
-           [15 2 0.25 0.8]]))
+  (r/atom [[ 1 1 1    1]   ; OTU 1
+           [ 4 6 0.5  1.5] ; OTU 2
+           [15 2 0.25 0.8] ; OTU 3
+           ]))
 
-(def protocol-otu-bias-total
-  (r/atom nil))
+(def num-otus 3)
+(def num-samples 2)
+
+;; (def protocol-otu-bias-total
+;;   (r/atom nil))
 
 (defn total-bias
   "Returns a vec with the total bias for each OTU."
@@ -63,6 +70,96 @@
 (defn event-val [event]
   (-> event .-target .-value))
 
+(defn counts-to-rel-abund [counts]
+  (vec (map #(/ % (reduce + counts)) counts)))
+
+(defn transpose
+  "Transpose a matrix (2d vector)"
+  [mat]
+  (apply map vector mat))
+
+(defn calc-rel-abund
+  "Given a 2d count vec with rows OTU, cols sample (entry ij is otu i
+  count in sample j), return a vec of relative abundances (entry ij is
+  otu i relative abundance in sample j)."
+  [counts]
+  (defn rabund [vec]
+    (let [total (apply + vec)]
+      (map #(/ % total) vec)))
+
+  (let [;; samples => 2d vec, rows are samples, cols are OTUs
+        samples (transpose counts)]
+    (transpose (map rabund samples))))
+
+(def total-bar-height 350)
+(defn calc-rect-coords
+  "rel-abunds is a vector of relative abundances for all the OTUs for
+  a single sample.  Some OTUs could have 0 relative abundance.  All
+  vals should run from 0 to 1."
+  [rel-abunds]
+  (let [heights (map #(* total-bar-height %) rel-abunds)]
+    (map-indexed (fn [i n]
+                   (if (zero? i)
+                     {:start 0 :height n}
+                     ;; Add up all the heights before this one
+                     {:start (apply + (take i heights))
+                      :height n}))
+                 heights)))
+
+(def otu-colors ["#004488" "#DDAA33" "#BB5566"])
+
+(defn rabund-bar [sample-idx rect-coords]
+  (map-indexed (fn [i coords]
+                 (let [x (* 100 (inc sample-idx))
+                       y (:start coords)
+                       width 50
+                       height (:height coords)]
+                   ^{:key (str x "-" y)}
+                   [:rect {:x x :y y
+                           :width width :height height
+                           :fill (otu-colors i)}]))
+               rect-coords))
+
+(defn bar-charts
+  "Both arguments are atoms."
+  [otu-counts otu-bias-per-step]
+  (let [rel-abund-by-sample (transpose (calc-rel-abund @otu-counts))
+        all-rect-coords (map calc-rect-coords rel-abund-by-sample)]
+
+    (print "@otu-counts")
+    (pp/pprint @otu-counts)
+
+    (print "(calc-rel-abund @otu-counts)")
+    (pp/pprint (calc-rel-abund @otu-counts))
+
+    (print "rel-abund-by-sample")
+    (pp/pprint rel-abund-by-sample)
+
+    (print "all-rect-coords")
+    (pp/pprint all-rect-coords)
+    ;; (print (map rabund-bar (range num-samples) all-rect-coords))
+    [:svg {:width 500 :height 500}
+     (map rabund-bar (range num-samples) all-rect-coords)]))
+
+
+
+#_(defn bar-charts [otu-counts otu-bias-per-step]
+  ;; Make sure any existing svg has been removed
+  (-> js/d3
+      (.select (str "#" html-id-composition-charts-svg))
+      .remove)
+  (let [svg (-> js/d3
+                (.select (str "#" html-id-composition-charts))
+                (.append "svg")
+                (.attr "id" html-id-composition-charts-svg)
+                (.attr "width" 500)
+                (.attr "height" 500))]
+    (-> svg
+        (.append "circle")
+        (.attr "r" 10)
+        (.attr "cx" 100)
+        (.attr "cy" 100))))
+
 ;;;; Components
 
 (defn bias-table-header []
@@ -74,6 +171,12 @@
     [:th "Sequencing bias"]
     [:th "Bioinformatics bias"]
     [:th "Total bias"]]])
+
+(defn check-NaN [event]
+  (let [val (js/parseFloat (event-val event))]
+    (if (js/isNaN val)
+      0
+      val)))
 
 (defn bias-table-body
   "Assumes all OTUs have equal number of steps."
@@ -91,11 +194,17 @@
             ^{:key (str "step-" step-idx)}
             [:td
              [:input {:type "text"
-                      :value (get-in @otu-bias-per-step [otu-idx step-idx])
-                      :on-change #(swap! otu-bias-per-step
-                                         assoc-in
-                                         [otu-idx step-idx]
-                                         (event-val %))}]]))
+                      :value (get-in @otu-bias-per-step
+                                     [otu-idx step-idx])
+                      :on-change (fn [event]
+                                   (let [val (check-NaN event)]
+                                     (swap! otu-bias-per-step
+                                            assoc-in
+                                            [otu-idx step-idx]
+                                            val)
+                                     (reset! observed-otu-counts
+                                             (observed-counts @actual-otu-counts
+                                                              @protocol-otu-bias-per-step))))}]]))
          [:td (reduce * (get @otu-bias-per-step otu-idx))]]))]))
 
 (defn bias-table [data]
@@ -110,7 +219,8 @@
     [:th "Sample 1"]
     [:th "sample 2"]]])
 
-(defn count-table-body
+;; parseFloat -> need to handle NaN
+(defn actual-count-table-body
   [otu-counts]
   (let [num-otus (count @otu-counts)
         num-samples (count (first @otu-counts))]
@@ -126,17 +236,22 @@
             [:td
              [:input {:type "text"
                       :value (get-in @otu-counts [otu-idx sample-idx])
-                      :on-change #(swap! otu-counts
-                                         assoc-in
-                                         [otu-idx sample-idx]
-                                         (event-val %))}]]))]))]))
+                      :on-change (fn [event]
+                                   (let [val (check-NaN event)]
+                                     (swap! otu-counts
+                                            assoc-in
+                                            [otu-idx sample-idx]
+                                            val)
+                                     (reset! observed-otu-counts
+                                             (observed-counts @actual-otu-counts
+                                                              @protocol-otu-bias-per-step))))}]]))]))]))
 
-(defn count-table [data]
+(defn actual-count-table [data]
   [:table
    [count-table-header]
-   [count-table-body data]])
+   [actual-count-table-body data]])
 
-(defn actual-count-table-body
+#_(defn observed-count-table-body
   "Assumes the data is all in the proper shape."
   [otu-counts otu-bias-per-step]
   (let [num-otus (count @otu-counts)
@@ -154,18 +269,52 @@
               ^{:key (str "sample-" sample-idx)}
               [:td (* count (get bias otu-idx))])))]))]))
 
-(defn actual-count-table [otu-counts otu-bias-per-step]
+(defn observed-count-table-body
+  "Assumes the data is all in the proper shape."
+  [otu-counts]
+  [:tbody
+     (doall
+      (for [otu-idx (range num-otus)]
+        ^{:key (str "otu-" otu-idx)}
+        [:tr
+         [:td (str "otu-" (inc otu-idx))]
+         (doall
+          (for [sample-idx (range num-samples)]
+            (let [count (get-in @otu-counts [otu-idx sample-idx])]
+              ^{:key (str "sample-" sample-idx)}
+              [:td count])))]))])
+
+(defn observed-count-table [otu-counts otu-bias-per-step]
   [:table
    [count-table-header]
-   [actual-count-table-body otu-counts otu-bias-per-step]])
+   [observed-count-table-body otu-counts otu-bias-per-step]])
+
+#_(defn update-charts []
+  [:input {:type "button"
+           :value "Update charts!"
+           :on-click (fn [e]
+                       (bar-charts actual-otu-counts
+                                        protocol-otu-bias-per-step))}])
 
 ;;;; Rendering
+
+(reset! observed-otu-counts
+        (observed-counts @actual-otu-counts
+                         @protocol-otu-bias-per-step))
 
 (r/render [bias-table protocol-otu-bias-per-step]
           (.getElementById js/document html-id-bias-table))
 
-(r/render [count-table actual-otu-counts]
+(r/render [actual-count-table actual-otu-counts]
           (.getElementById js/document html-id-otu-table-actual-counts))
 
-(r/render [actual-count-table actual-otu-counts protocol-otu-bias-per-step]
-          (.getElementById js/document html-id-otu-table-observed-counts))
+(r/render [observed-count-table
+           observed-otu-counts]
+          (.getElementById js/document
+                           html-id-otu-table-observed-counts))
+
+(r/render [bar-charts actual-otu-counts observed-otu-counts]
+          (.getElementById js/document html-id-composition-charts))
+
+;; (r/render [update-charts]
+;;           (.getElementById js/document html-id-update-charts-button))
